2ab2e69674877b22d9e2ab84f592da0ad8623cf9
iamamit9 <akumarsingh330@gmail.com>
2025-05-30 15:55:08 +0530

    Javascript task completed

---
+++ Javascript-task/Task-2/js/script.js
@@ -0,0 +1,19 @@
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function sumPrimes(start, end) {
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    if (isPrime(i)) {
+      sum += i;
+    }
+  }
+  console.log(`Sum of primes between ${start} and ${end} is ${sum}.`);
+}
+
+sumPrimes(21, 50);
